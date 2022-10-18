da606d9fe3ed5b1d707c306039b25bca00749ec7
Kuein <kuein@outlook.com>
2022-10-18 10:07:13 +0200

    Initial

---
+++ joiner/go.mod
@@ -0,0 +1,10 @@
+require (
+	github.com/aws/aws-lambda-go v1.34.1
+	github.com/buger/jsonparser v1.1.1 // indirect
+)
+
+replace gopkg.in/yaml.v2 => gopkg.in/yaml.v2 v2.2.8
+
+module joiner
+
+go 1.16
---
+++ joiner/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"strconv"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/buger/jsonparser"
+)
+
+var telegramApi string = "https://api.telegram.org/bot" + os.Getenv("TELEGRAM_BOT_TOKEN") + "/deleteMessage"
+
+func handler(request events.LambdaFunctionURLRequest) {
+	payload := []byte(request.Body)
+	_, err := jsonparser.GetString(payload, "message", "new_chat_participant")
+	if err != nil {
+		return
+	}
+	chat, err := jsonparser.GetInt(payload, "message", "chat", "id")
+	if err != nil {
+		log.Fatal(err)
+	}
+	messageID, err := jsonparser.GetInt(payload, "message", "message_id")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// delete message
+	_, err = http.PostForm(
+		telegramApi,
+		url.Values{
+			"chat_id":    {strconv.Itoa(int(chat))},
+			"message_id": {strconv.Itoa(int(messageID))},
+		})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func main() {
+	lambda.Start(handler)
+}
